cbe9cc50d67795010e4cbf356897758da2cb9f3a
Haydart <makowiecki.radoslaw@gmail.com>
2017-09-15 20:30:34 +0200

    Add logging  & basic rx Kotlin extensions

---
+++ app/src/main/java/pl/rmakowiecki/smartalarm/extensions/RxExtensions.kt
@@ -0,0 +1,19 @@
+package pl.rmakowiecki.smartalarm.extensions
+
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.schedulers.Schedulers
+
+fun <T> Observable<T>.applyIoSchedulers(): Observable<T> =
+        subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+fun <T> Single<T>.applyIoSchedulers(): Single<T> =
+        subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+
+infix fun CompositeDisposable.handle(disposable: Disposable) {
+    this += disposable
+}
