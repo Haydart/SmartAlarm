b6ef27a6bbf2d23a0133c5779d4566d170ca218a
Haydart <makowiecki.radoslaw@gmail.com>
2017-09-15 21:13:58 +0200

    Revert making the companion app an USB host

--- app/src/main/java/pl/rmakowiecki/smartalarm/MainActivity.kt
+++ app/src/main/java/pl/rmakowiecki/smartalarm/MainActivity.kt
@@ -1,7 +1,7 @@
 package pl.rmakowiecki.smartalarm
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
