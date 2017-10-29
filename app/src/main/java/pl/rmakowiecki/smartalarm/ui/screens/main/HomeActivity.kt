90f10138f7da65581c83e299141f1b2d3b2c7cd9
Haydart <makowiecki.radoslaw@gmail.com>
2017-10-29 13:46:54 +0100

    Setup bottom navigation bar items

--- app/src/main/java/pl/rmakowiecki/smartalarm/ui/screens/main/HomeActivity.kt
+++ app/src/main/java/pl/rmakowiecki/smartalarm/ui/screens/main/HomeActivity.kt
@@ -1,21 +1,29 @@
 package pl.rmakowiecki.smartalarm.ui.screens.main
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
 import kotlinx.android.synthetic.main.activity_home.*
 import pl.rmakowiecki.smartalarm.R
-import pl.rmakowiecki.smartalarm.extensions.startActivity
-import pl.rmakowiecki.smartalarm.ui.screens.auth.AuthActivity
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        logoutButton.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            startActivity<AuthActivity>()
-        }
+
+        bottomNavigationBar.accentColor = ContextCompat.getColor(this, R.color.accent)
+
+        val stateItem = AHBottomNavigationItem(R.string.bottom_bar_state, R.drawable.ic_settings_power_white_24px, R.color.primary)
+        val historyItem = AHBottomNavigationItem(R.string.bottom_bar_history, R.drawable.ic_history_white_24px, R.color.primary)
+        val settingsItem = AHBottomNavigationItem(R.string.bottom_bar_settings, R.drawable.ic_settings_white_24px, R.color.primary)
+
+        bottomNavigationBar.addItem(stateItem)
+        bottomNavigationBar.addItem(historyItem)
+        bottomNavigationBar.addItem(settingsItem)
+
+        bottomNavigationBar.titleState = AHBottomNavigation.TitleState.ALWAYS_HIDE
     }
 }
