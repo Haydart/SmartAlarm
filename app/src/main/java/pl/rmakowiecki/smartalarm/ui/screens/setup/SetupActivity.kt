e6345eebfbfbf6e293ed1bf06fc2d5e6ad75b820
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-10 20:12:37 +0100

    Turn off hardware acceleration for dashed line view

--- app/src/main/java/pl/rmakowiecki/smartalarm/ui/screens/setup/SetupActivity.kt
+++ app/src/main/java/pl/rmakowiecki/smartalarm/ui/screens/setup/SetupActivity.kt
@@ -3,7 +3,8 @@ package pl.rmakowiecki.smartalarm.ui.screens.setup
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_auth.*
+import android.view.View
+import kotlinx.android.synthetic.main.activity_setup.*
 import pl.rmakowiecki.smartalarm.R
 import pl.rmakowiecki.smartalarm.ui.customView.TilingDrawable
 
@@ -13,10 +14,11 @@ class SetupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup)
         setActivityBackground()
+        dottedLine.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
     }
 
     private fun setActivityBackground() {
-        val rawDrawable = ContextCompat.getDrawable(this, R.drawable.background_vector)
+        val rawDrawable = ContextCompat.getDrawable(this, R.drawable.overlay_pattern_background_vector)
 
         val tilingDrawable = TilingDrawable(rawDrawable)
         headerBackgroundImage.background = tilingDrawable
