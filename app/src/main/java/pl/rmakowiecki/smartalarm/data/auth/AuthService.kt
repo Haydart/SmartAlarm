c037d92ab018756d23456f30674c6d5d52b8781f
Haydart <makowiecki.radoslaw@gmail.com>
2017-12-07 17:55:27 +0100

    Divide auth classes according to data layer

---
+++ app/src/main/java/pl/rmakowiecki/smartalarm/data/auth/AuthService.kt
@@ -0,0 +1,79 @@
+package pl.rmakowiecki.smartalarm.data.auth
+
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.messaging.FirebaseMessaging
+import io.reactivex.Single
+import pl.rmakowiecki.smartalarm.ui.screens.auth.LoginCredentials
+import pl.rmakowiecki.smartalarm.ui.screens.auth.RegisterCredentials
+import pl.rmakowiecki.smartalarm.ui.screens.auth.RemindPasswordCredentials
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FirebaseAuthService @Inject constructor() : AuthService {
+
+    private val rootDatabaseNode = FirebaseDatabase
+            .getInstance()
+            .reference
+
+    override fun isUserLoggedIn(): Single<Boolean> = Single.just(
+            FirebaseAuth.getInstance().currentUser != null
+    )
+
+    override fun login(credentials: LoginCredentials): Single<AuthResponse> = Single.create { emitter ->
+        FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(credentials.email, credentials.password)
+                .addOnCompleteListener {
+                    FirebaseMessaging.getInstance().subscribeToTopic("SgVIHNDQwsPj3lmS2jS1gS9Xz5r1") //todo pass on-hardcoded core device UID
+                    emitter.onSuccess(AuthResponse(it.isSuccessful, it.exception))
+                }
+    }
+
+    override fun register(credentials: RegisterCredentials): Single<AuthResponse> = Single.create { emitter ->
+        FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(credentials.email, credentials.password)
+                .addOnCompleteListener {
+                    emitter.onSuccess(AuthResponse(it.isSuccessful, it.exception))
+                }
+    }
+
+    override fun resetPassword(credentials: RemindPasswordCredentials): Single<AuthResponse> = Single.create { emitter ->
+        FirebaseAuth.getInstance()
+                .sendPasswordResetEmail(credentials.email)
+                .addOnCompleteListener {
+                    emitter.onSuccess(AuthResponse(it.isSuccessful, it.exception))
+                }
+    }
+
+    override fun signInWithFacebook(credential: AuthCredential): Single<AuthResponse> = Single.just(
+            AuthResponse(false, Throwable())
+    )
+
+    override fun signInWithGoogle(credential: AuthCredential): Single<AuthResponse> = Single.just(
+            AuthResponse(false, Throwable())
+    )
+
+    override fun logoutUser(): Single<Boolean> {
+        FirebaseAuth.getInstance().signOut()
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("SgVIHNDQwsPj3lmS2jS1gS9Xz5r1") //todo pass on-hardcoded core device UID
+
+        return Single.just(true)
+    }
+}
+
+interface AuthService {
+    fun login(credentials: LoginCredentials): Single<AuthResponse>
+    fun register(credentials: RegisterCredentials): Single<AuthResponse>
+    fun resetPassword(credentials: RemindPasswordCredentials): Single<AuthResponse>
+    fun signInWithFacebook(credential: AuthCredential): Single<AuthResponse>
+    fun signInWithGoogle(credential: AuthCredential): Single<AuthResponse>
+    fun isUserLoggedIn(): Single<Boolean>
+    fun logoutUser(): Single<Boolean>
+}
+
+class AuthResponse(
+        val isSuccessful: Boolean,
+        val error: Throwable?
+)
